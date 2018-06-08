6406fb12363b9f4e81ffca2b286f9a0c4f717349
tzongw <tzongw@gmail.com>
2018-06-09 01:31:26 +0800

    .vim

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-
+cd
 ln -s .dotfiles/.gitignore_global .agignore
 ln -s .dotfiles/.gitignore_global .
 ln -s .dotfiles/..gitconfig .
@@ -8,4 +8,9 @@ ln -s .dotfiles/.vimrc .
 ln -s .dotfiles/.vimrc.bundles .
 ln -s .dotfiles/.vimrc.bundles.local .
 ln -s .dotfiles/.vimrc.local .
-grep dotfiles .zshrc || echo 'source .dotfiles/.profile' >> .zshrc
+grep dotfiles .zshrc > /dev/null || echo 'source .dotfiles/.profile' >> .zshrc
+mkdir .vim
+mkdir .vim/bundle
+mkdir .vim/colors
+ln -s .dotfiles/molokai.vim .vim/colors
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
